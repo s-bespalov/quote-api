e045a1787865f3e7a606b07cc92d65902b40c064
Sergey Bespalov <sergey.bespalov@protonmail.com>
2020-03-30 19:30:33 +0300

    base project

--- server.js
+++ server.js
@@ -8,3 +8,35 @@ const PORT = process.env.PORT || 4001;
 
 app.use(express.static('public'));
 
+app.get('/api/quotes/random', (req, res, next) => {
+  res.send({ quote: getRandomElement(quotes)});
+});
+
+app.get('/api/quotes', (req, res, next) => {
+  if (!req.query.person) {
+    res.send({ quotes: quotes });
+    return;
+  }
+
+  const personalQuotes = quotes
+    .filter(quote => quote.person === req.query.person);
+
+  res.send({quotes: personalQuotes});
+})
+
+app.post('/api/quotes', (req, res, next) => {
+  if (!req.query.person || !req.query.quote) {
+    res.status(400).send();
+    return;
+  }
+
+  const newQuote = {
+    quote: req.query.quote,
+    person: req.query.person
+  }
+
+  quotes.push(newQuote);
+  res.status(201).send({ quote: newQuote });
+});
+
+app.listen(PORT);
